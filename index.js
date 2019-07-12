0c13e4ad82453137bdf3a89ab4d7f46c0357d10c
Le H Ngo <seiji@o2.pl>
2019-07-12 20:41:14 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let empty = [];
+    for (let i = 0; i < array.length; i++) {
+        empty.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+
+    }
+    return empty;
+}
+
+
+function countdown(number) {
+    let n = number
+    while (n > -1) {
+        console.log(n)
+        n -= 1
+    }
+}
